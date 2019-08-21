33a1d29ea118192302360e5442769d3e2512a9f0
bomibear <bomibearly@gmail.com>
2019-08-21 11:50:20 -0700

    data can be saved

--- src/main/java/com/bomibear/songr/controllers/AlbumController.java
+++ src/main/java/com/bomibear/songr/controllers/AlbumController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -29,5 +31,12 @@ public class AlbumController {
         m.addAttribute("albums", albums);
 
         return "allAlbums";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbums(String title, String artist, int songCount, double length, String imageUrl){
+        Album album = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(album);
+        return new RedirectView("/albums");
     }
 }
--- src/main/java/com/bomibear/songr/models/Album.java
+++ src/main/java/com/bomibear/songr/models/Album.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Album {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     String title;
     String artist;
